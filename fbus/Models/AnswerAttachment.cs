d2ee73ac6812c1a2b3e44ddf9c17e72321794b77
Ivan Krivokon <ivan.krivokon@gmail.com>
2020-12-31 14:07:48 +0300

    s

---
+++ fbus/Models/AnswerAttachment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace fbus.Models
+{
+    public class AnswerAttachment
+    {
+        public Guid Id { get; set; }
+        public Guid AnswerId { get; set; }
+        public DateTime Created { get; set; }
+        public String FileName { get; set; }
+        public String MimeType { get; set; }
+        public Int32 Size { get; set; }
+    }
+
+}
---
+++ fbus/Models/AnswerEvent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace fbus.Models
+{
+    public enum AnswerEventTypeEnum
+    {
+        Click = 1,
+        Move,
+        Drag,
+        Press,
+        Other
+    }
+    public class AnswerEvent
+    {
+        public Guid Id { get; set; }
+        public Guid AnswerId { get; set; }
+        public DateTime Created { get; set; }
+        public String Value { get; set; }
+        public AnswerEventTypeEnum Type { get; set; }
+
+    }
+
+}
